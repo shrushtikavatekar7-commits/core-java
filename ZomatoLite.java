eed0ed02a0c312a8980feec12aef2c52a49035ff
shrushti kavatekar <shrushtikavatekar7@gmail.com>
2026-03-06 10:19:30 +0530

    Add files via upload

---
+++ ZomatoLite.java
@@ -0,0 +1,14 @@
+class ZomatoLite{
+
+public static String searchRestaurant(String foodNames){
+String resturantName[] = null;
+
+if(foodName == "Burger"){
+      resturantName = {"Raj Hotel", "Emperor Legacy"};
+return
+}else{
+  System.out.println(foodName + "Not Found"};
+  }
+  return resturantName;
+  }
+  }
